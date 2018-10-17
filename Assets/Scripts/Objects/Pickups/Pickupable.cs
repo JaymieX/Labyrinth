c895f1c00e002cf0d9d308bd00e8379b60e519ad
JakeNull <jake9828@gmail.com>
2018-10-17 00:54:26 -0400

    Added Powerup

---
+++ Assets/Scripts/Objects/Pickups/Pickupable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    public enum PickupType
+    {
+        Invincibility
+    }
+
+    public PickupType Type;
+    public GameObject GraphicEffect;
+
+    private ICommand _pickupAction;
+
+    void Awake()
+    {
+        switch (Type)
+        {
+            case PickupType.Invincibility:
+                _pickupAction = new InvincibilityPickup();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _pickupAction.Execute();
+            StartCoroutine("PlayEffect");
+        }
+    }
+
+    IEnumerator PlayEffect()
+    {
+        GraphicEffect = Instantiate(GraphicEffect, new Vector3(transform.position.x, 0f, transform.position.z), Quaternion.identity);
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+
+        yield return new WaitForSeconds(2f);
+        Destroy(GraphicEffect);
+        Destroy(this.gameObject);
+    }
+}
