f54278c84b06834ab3fed948ac46d14623e129b3
JakeNull <jake9828@gmail.com>
2018-10-17 21:11:11 -0400

    Added Second character

--- Assets/Scripts/Characters/AI/CrawlerController.cs
+++ Assets/Scripts/Characters/AI/CrawlerController.cs
@@ -6,34 +6,49 @@ public class CrawlerController : MonoBehaviour, IMonsterController
 {
     private bool _inAttackRange;
     private bool _dead;
+    private bool _trackPlayer;
     private CharacterController _cc;
     private Animator _ani;
 
     // Use this for initialization
     void Start()
     {
+        _trackPlayer = false;
         _inAttackRange = false;
         _dead = false;
         _cc = GetComponent<CharacterController>();
         _ani = GetComponent<Animator>();
-
-        _ani.SetBool("FoundPlayer", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Activate player tracking
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            _trackPlayer = true;
+            _ani.SetBool("FoundPlayer", true);
+        }
+
         if (_dead) return;
 
-        if (!_inAttackRange)
-        {
-            _ani.SetBool("PlayerInRange", false);
 
-            LocalTrackPlayer();
-        }
-        else
+        if (_trackPlayer)
         {
-            Attack();
+            LookTowardsPlayer();
+            if (!_inAttackRange)
+            {
+                _ani.SetBool("PlayerInRange", false);
+
+                if (_ani.GetCurrentAnimatorStateInfo(0).IsName("Zombie Walk"))
+                {
+                    LocalTrackPlayer();
+                }
+            }
+            else
+            {
+                Attack();
+            }
         }
     }
 
@@ -46,9 +61,8 @@ public class CrawlerController : MonoBehaviour, IMonsterController
     {
         var offset = GetOffset();
         offset = new Vector3(offset.x, 0.0f, offset.z);
-        Quaternion rotation = Quaternion.LookRotation(offset);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2.0f);
+        LookTowardsPlayer();
 
         if (offset.magnitude > 1.0f)
         {
@@ -84,5 +98,14 @@ public class CrawlerController : MonoBehaviour, IMonsterController
     private Vector3 GetOffset()
     {
         return PlayerManager.Instance.PlayerPos - transform.position;
+    }
+
+    private void LookTowardsPlayer()
+    {
+        var offset = GetOffset();
+        offset = new Vector3(offset.x, 0.0f, offset.z);
+        Quaternion rotation = Quaternion.LookRotation(offset);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2.0f);
     }
 }
