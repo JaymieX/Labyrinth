389915423395fd3adfeb691cf05b4c2c3d9aba7e
Jake-Laptop <jake9828@gmail.com>
2018-10-31 18:59:44 -0400

    WIP hover ui above enemy

---
+++ Assets/Scripts/Characters/AI/MonsterUiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterUiController : MonoBehaviour
+{
+    private GameObject _uiCanvas;
+    public GameObject UI;
+
+    private GameObject _spawnedUI;
+    // Use this for initialization
+    void Start()
+    {
+        _uiCanvas = GameObject.FindWithTag("EnemyUICanvas");
+
+        _spawnedUI = Instantiate(UI, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity);
+        _spawnedUI.transform.SetParent(_uiCanvas.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _spawnedUI.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+    }
+}
