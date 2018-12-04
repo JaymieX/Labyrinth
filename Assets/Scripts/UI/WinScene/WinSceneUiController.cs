443e0fb4996c1dbc175fc68f3a1318802e5f987e
JaymieX <jake9828@gmail.com>
2018-12-04 16:51:19 -0500

    Added winning scene

---
+++ Assets/Scripts/UI/WinScene/WinSceneUiController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinSceneUiController : MonoBehaviour
+{
+    public Text WinningIdText;
+    public Text WinningGearText;
+
+    // Use this for initializations
+    void Start()
+    {
+        WinningIdText.text = WinnerManager.WinnerId.ToString();
+        WinningGearText.text = WinnerManager.Gears.ToString();
+    }
+
+    public void OnMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
